fb9f942622e9c7d21c12b106bb7bfd470c2f172f
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-02-08 20:24:03 +0000

    [ref] set default db port to 4600

--- api/config/db.go
+++ api/config/db.go
@@ -10,4 +10,4 @@ var DB_USER = os.Getenv("POSTGRES_USER")
 var DB_HOST = os.Getenv("POSTGRES_HOST")
 var DB_PASS = os.Getenv("POSTGRES_PASSWORD")
 var DB_NAME = os.Getenv("POSTGRES_DATABASE")
-var DB_PORT = 5432
+var DB_PORT = 4600
