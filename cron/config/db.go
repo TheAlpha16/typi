65a558509fb1ed2e132353ddee51f62fc702d97b
TheAlpha16 <uddagirijithendranadh@gmail.com>
2025-02-09 12:26:38 +0000

    [fix] keep postgres port to be dynamic

--- cron/config/db.go
+++ cron/config/db.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"os"
+	"strconv"
 
 	_ "github.com/joho/godotenv/autoload"
 )
@@ -10,4 +11,13 @@ var DB_USER = os.Getenv("POSTGRES_USER")
 var DB_HOST = os.Getenv("POSTGRES_HOST")
 var DB_PASS = os.Getenv("POSTGRES_PASSWORD")
 var DB_NAME = os.Getenv("POSTGRES_DATABASE")
-var DB_PORT = 4600
+
+var DB_PORT int
+
+func init() {
+	var err error
+	DB_PORT, err = strconv.Atoi(os.Getenv("POSTGRES_PORT"))
+	if err != nil {
+		DB_PORT = 4600
+	}
+}
